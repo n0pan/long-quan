fd129f46539397ce1a0bec28849ab88eed70fd14
LQ Hoang <longquanhp@gmail.com>
2021-07-03 16:16:47 -0400

    upgraded to next11 and react

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "60e7246e9c4370fa9f7d"; }
+/******/ 		__webpack_require__.h = function() { return "aa381429f6b00a41608c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
