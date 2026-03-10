2ba9a20888b2408290cdcea49e48d348116bfd44
LQ Hoang-Pham <longquanhp@gmail.com>
2026-03-10 17:19:10 -0400

    chore: centered content

--- src/app/components/main-layout.tsx
+++ src/app/components/main-layout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 
 function MainLayout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return <div className="mx-auto max-w-6xl px-4 sm:px-6">{children}</div>;
 }
 
 export default MainLayout;
